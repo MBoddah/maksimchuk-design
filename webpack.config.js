8887d3011c856b708d0e1e6b4ce9c125a0792883
Dmitry Morozov <mboddah@yandex.ru>
2021-04-21 21:05:27 +0300

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watch: true,
+
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [['@babel/preset-env', {
+                        debug: true,
+                        corejs: 3,
+                        useBuiltIns: "usage"
+                    }]]
+                }
+            }
+        },
+        {
+            test:/\.(s*)css$/,
+            use: [
+                miniCss.loader,
+                'css-loader',
+                'sass-loader',
+            ]
+        }]
+    },
+    plugins: [
+        new miniCss({
+            filename: 'style.css',
+        }),
+    ]
+};
