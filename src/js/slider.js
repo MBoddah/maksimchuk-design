a4440e1e6baee203bd85aab79968d6ebe6c2a269
Dmitry Morozov <mboddah@yandex.ru>
2021-05-14 23:38:48 +0300

    correct gram

--- src/js/slider.js
+++ src/js/slider.js
@@ -132,7 +132,7 @@ function slider( {
     })
 
        //Activate swipes
-        sliderInner.addEventListener('touchstart', (event) => {
+    sliderInner.addEventListener('touchstart', (event) => {
         event.preventDefault;
         startingSwipeX = getStartingSwipeX(event);
         if (activateAutoTurning === true) {
